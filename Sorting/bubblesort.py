5b24bfe6a7473b144b5dc01eb93171549575c54b
Himanshu <31452165+h96Coder@users.noreply.github.com>
2020-02-18 09:02:56 +0530

    Update bubblesort.py

--- Sorting/bubblesort.py
+++ Sorting/bubblesort.py
@@ -1,1 +1,12 @@
-
+def bubblesort(A):
+    k=len(A)
+    for j in range(k,-1,-1):
+        for i in range(0,j-1):
+            if(A[i]>A[i+1]):
+                temp=A[i]
+                A[i]=A[i+1]
+                A[i+1]=temp
+        k=k-1
+    return A
+A=[5,4,10,2,11]
+print(bubblesort(A))
